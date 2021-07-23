8959ccb9664396a2dda5da02c42f333552236a55
pi <pi@vm>
2021-07-23 14:02:27 -0400

    venmo....

--- lib/nomadic/venmo.rb
+++ lib/nomadic/venmo.rb
@@ -15,12 +15,8 @@ module NOMADIC
         a << "txn=pay"
       end
       a << "recipients=#{@h[:to]}"
-      a << "amount=#{@h[:amount]}"
-      if @h.has_key? :invoice
-        a << "note=#{URI.encode(@h[:invoice])}"
-      else
-        a << "note=#{URI.encode(@h[:note] || 'thanks.')}"
-      end
+      a << "amount=#{@h[:amt]}"
+      a << "note=#{@h[:note]}"
       r << a.join('&')
       return r.join('')
     end
